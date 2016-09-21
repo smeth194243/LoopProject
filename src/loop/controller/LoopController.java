ed0a5ef6a824d2b9fae0817e5cbf4aeda5e16509
smeth194243 <smor7432@740s-pl-114.local>
2016-09-21 11:27:23 -0600

    Things were done.

--- src/loop/controller/LoopController.java
+++ src/loop/controller/LoopController.java
@@ -2,5 +2,40 @@ package loop.controller;
 
 public class LoopController
 {
-
+	
+	private Scanner keyboardInput;
+	public LoopController();
+	{
+		keyboardInput = new Scanner(System.in);
+	}
+	
+	
+	public void start()
+	{
+		System.out.println("Today, we're expirimenting with loops!");
+		System.out.println("Here we go!"); 
+		
+		System.out.println("Do you like Computer programming?");
+		String answer = keyboardInput.nextLine();
+		
+		if(answer.equalsIgnoreCase("Yes"));
+		{
+			System.out.println("Me too! Who is your teacher?");
+			
+			if (answer.equalsIgnoreCase("Mr. Henrichsen"));
+			{
+				System.out.println("Isn't he the funniest?");
+				
+				if (answer.equalsIgnoreCase("Yeah"));
+				{
+					System.out.println("His jokes always make me laugh.");
+				}
+				else 
+				{
+					System.out.println("Is he a good teacher?");
+				}
+			}
+		}
+		
+	}
 }
--- src/loop/controller/LoopRunner.java
+++ src/loop/controller/LoopRunner.java
@@ -2,5 +2,10 @@ package loop.controller;
 
 public class LoopRunner
 {
-
+	
+	public static void main(String[] args)
+	{
+		LoopController loopAppController = new LoopController();
+		loopAppController.start();
+	}
 }
